072336c838f61159b9e48d2bbdbee0272b9cec2d
Edoardo Luppi <lp.edoardo@gmail.com>
2017-10-26 22:36:37 +0200

    Rethinked user mapper

--- src/main/java/it/reference/website/models/mappers/UserMapper.java
+++ src/main/java/it/reference/website/models/mappers/UserMapper.java
@@ -2,10 +2,9 @@ package it.reference.website.models.mappers;
 
 import it.reference.website.entities.security.User;
 import it.reference.website.models.UserModel;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -15,14 +14,14 @@ public abstract class UserMapper
    @Autowired
    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-   @Mapping(target = "password", ignore = true)
+   @Mapping(target = "password", qualifiedByName = "encodedPassword")
    public abstract User userModelToUser(final UserModel userModel);
 
    @Mapping(target = "password", ignore = true)
    public abstract UserModel userToUserModel(final User user);
 
-   @AfterMapping
-   protected void setEncodedPassword(final UserModel userModel, @MappingTarget final User user) {
-      user.setPassword(bCryptPasswordEncoder.encode(userModel.getPassword()));
+   @Named("encodedPassword")
+   public String getEncodedPassword(final String password) {
+      return bCryptPasswordEncoder.encode(password);
    }
 }
