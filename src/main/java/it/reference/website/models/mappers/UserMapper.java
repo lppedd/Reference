a169a8c08e4c976ce22ec2f9b3de1c8830a715bd
Edoardo Luppi <lp.edoardo@gmail.com>
2017-10-27 23:20:54 +0200

    Rethinked mappers structure

--- src/main/java/it/reference/website/models/mappers/UserMapper.java
+++ src/main/java/it/reference/website/models/mappers/UserMapper.java
@@ -2,26 +2,27 @@ package it.reference.website.models.mappers;
 
 import it.reference.website.entities.security.User;
 import it.reference.website.models.UserModel;
+import it.reference.website.models.mappers.utils.StringMapperUtils;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = StringMapperUtils.class)
 public abstract class UserMapper
 {
    @Autowired
    private BCryptPasswordEncoder bCryptPasswordEncoder;
    
-   @Mapping(target = "password", qualifiedByName = "encodedPassword")
+   @Mapping(target = "password", qualifiedByName = "encodePassword")
    public abstract User userModelToUser(final UserModel userModel);
    
    @Mapping(target = "password", ignore = true)
    public abstract UserModel userToUserModel(final User user);
    
-   @Named("encodedPassword")
-   public String getEncodedPassword(final String password) {
+   @Named("encodePassword")
+   public String encodePassword(final String password) {
       return bCryptPasswordEncoder.encode(password);
    }
 }
---
+++ src/main/java/it/reference/website/models/mappers/utils/StringMapperUtils.java
@@ -0,0 +1,8 @@
+package it.reference.website.models.mappers.utils;
+
+public class StringMapperUtils
+{
+   public static String trim(final String value) {
+      return value == null ? null : value.trim();
+   }
+}
